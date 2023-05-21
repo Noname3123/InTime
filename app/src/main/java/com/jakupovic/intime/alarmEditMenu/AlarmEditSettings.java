457fa5ebd16b7cdd32828d2554253fa052be8d1f
bjakupo <bjakupo@gmail.com>
2023-05-21 09:40:08 +0200

    Began visual design of Alarm edit/create activity, check AndroidManifest.xml for possible errors

---
+++ app/src/main/java/com/jakupovic/intime/alarmEditMenu/AlarmEditSettings.java
@@ -0,0 +1,16 @@
+package com.jakupovic.intime.alarmEditMenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jakupovic.intime.R;
+
+public class AlarmEditSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_edit_settings);
+    }
+}
